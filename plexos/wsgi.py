ce7ca3eaf4f4b00636f2bdb1ce81a66c5abe529f
Steven Broad <steven.broad@energyexemplar.com>
2020-05-23 21:34:03 -0700

    Update wsgi path

--- plexos/wsgi.py
+++ plexos/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/..')
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../plexos')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plexos.settings")
 
 application = get_wsgi_application()
